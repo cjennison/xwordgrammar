b5f5de1e9594cb03e3fe0a511a7e9f1b00f84e16
Christopher Jennison <cjennison92@gmail.com>
2013-05-18 15:14:29 -0700

    Index Page

---
+++ trunk/scripts/navigator.js
@@ -0,0 +1,38 @@
+var navItems;
+var selector;
+
+function initNav(page){
+	navItems = $("#navigation ul li");
+	selector = $("#nav-selector");
+	console.log(navItems);
+	
+	for(var i = 0;i < navItems.length;i++){
+		//Place the navigator on the correct item
+		if($(navItems[i]).html() == page){
+			switch(page){
+				case "Home":
+					$(selector).css("left", "-19px")
+					break;
+			}
+		}
+		
+		//Add event listeners
+		$(navItems[i]).mouseover(function(){
+			switch($(this).html()){
+				case "Home":
+					$(selector).css("left", "-19px")
+					break;
+				
+				case "Books":
+					$(selector).css("left", "152px")
+					break;
+				
+				case "About":
+					$(selector).css("left", "328px")
+					break;
+			}
+		})
+	}
+	
+	
+}
